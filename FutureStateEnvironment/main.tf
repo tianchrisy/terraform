8dba31ef911dfae7a9f3ddaa77aa8cdf3b353e72
tianchrisy <tianchrisy@gmail.com>
2024-10-24 08:55:54 -0400

    Moving Projects to Github

---
+++ FutureStateEnvironment/main.tf
@@ -0,0 +1,39 @@
+resource "azurerm_resource_group" "practiceName_RG" {
+  name     = "example-resources"
+  location = "Central US"
+
+  tags = {
+    environment = "practiceInfrastructure"
+  }
+}
+
+resource "azurerm_virtual_machine" "virtualMachineDC1" {
+  name                = "virtualMachineDC1"
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.practiceName_RG.name
+  vm_size             = "Standard_E4s_v3"
+
+  storage_os_disk {
+    name              = var.virtualMachineDC1Name + "-osdisk"
+    caching           = "ReadWrite"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = 127
+  }
+  os_profile {
+    computer_name  = "DC1"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  tags = {
+    environment = "practiceInfrastructure"
+  }
+
+
+}
---
+++ FutureStateEnvironment/variables.tf
@@ -0,0 +1,14 @@
+variable "admin_password" {
+  type      = string
+  sensitive = true
+  default   = "dsfgbsr#$#$ergdf5tghTTTHdfvxc##vsdfsdtgeDDEEDSe##@2224445667u57"
+}
+variable "admin_username" {
+  type      = string
+  sensitive = true
+  default   = "sdpadmin"
+}
+variable "virtualMachineDC1Name"{
+  type = string
+  default = "ThisPractice"
+}
